7f2c2dbaee38075772d75e95c8fa130ed919c830
ADAMI VALENTINA <30869@ds.units.it>
2018-11-19 16:26:48 +0100

    mancava error

--- index.js
+++ index.js
@@ -189,7 +189,19 @@ app.get('/', function(req, res, next) {
             .getResponse();
         }
       };
-    
+      const ErrorHandler = {
+        canHandle() {
+          return true;
+        },
+        handle(handlerInput, error) {
+          console.log(`Error handled: ${error.message}`);
+      
+          return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+        },
+      };
       const SessionEndedRequestHandler = {
         canHandle(handlerInput) {
           return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
